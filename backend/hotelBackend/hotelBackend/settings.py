c6793bb7fb2d56426e8a6e06d5d802afe028255e
bsra21 <bm.busra14@gmail.com>
2026-02-07 11:05:06 +0300

    setting edits

--- backend/hotelBackend/hotelBackend/settings.py
+++ backend/hotelBackend/hotelBackend/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-%zmc(i!q843^pvdxhwx#jfno77(w)-2)idk5#4lf4ik)$_z4^)'
+SECRET_KEY = 'django-insecure-%zmc(i!q843^pvdxhwx#jfno77(w)-2)idk5#4lf4ik)$_z4^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')  # fallback dev-key
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
+#SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')  # fallback dev-key
+#DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,7 +117,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 #}
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
