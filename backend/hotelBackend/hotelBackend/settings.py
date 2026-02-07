c044ca6b609d544534773a5a46adf753b3511432
bsra21 <bm.busra14@gmail.com>
2026-02-07 12:16:42 +0300

    Pin Python 3.11 and fix PostgreSQL driver

--- backend/hotelBackend/hotelBackend/settings.py
+++ backend/hotelBackend/hotelBackend/settings.py
@@ -117,7 +117,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 #}
 
 DATABASES = {
-     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+   #  'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+ "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
